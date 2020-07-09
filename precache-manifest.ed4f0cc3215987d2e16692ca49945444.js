d9f5074b9eb1de11b2167f3a46361388605b9a98
myusername <myemail>
2020-07-09 13:38:27 -0700

    Updates

---
+++ precache-manifest.ed4f0cc3215987d2e16692ca49945444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccb12a1f69041658b8e992608e77a5b",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/index.html"
+  },
+  {
+    "revision": "613b7edaf8edbc71733e",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/static/css/main.6f5268c3.chunk.css"
+  },
+  {
+    "revision": "bf660454313798bd9bea",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/static/js/2.722d449b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/static/js/2.722d449b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613b7edaf8edbc71733e",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/static/js/main.1744dc9a.chunk.js"
+  },
+  {
+    "revision": "fb16504ebd1eb802e18a",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/static/js/runtime-main.448f25a3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/beingfaisalmalik.github.io/tracker-covid/static/media/image.d7265326.png"
+  }
+]);
